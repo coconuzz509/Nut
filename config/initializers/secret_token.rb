021c7d5610a8dafa336cdc889dd9ce8825a7d66b
coconuzz509 <godonlyknows509@gmail.com>
2012-06-06 11:37:42 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nut::Application.config.secret_token = '5980ea2f15e3779e253afa3b537093db5fd5fa3e2a9654c169c982c5667595fd3266cb43ed6e3f2b7ef800d3bdd9b1453c2a527bd309d5f0d4621d30970d186a'
